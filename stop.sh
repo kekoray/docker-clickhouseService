c5bdb46b4b6b1d41130f3c6c1a658fc1b8278265
kekoray <61939347+kekoray@users.noreply.github.com>
2023-07-07 14:05:42 +0800

    1

---
+++ stop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 获取本机架构信息
+arch=$(uname -m)
+
+# 根据架构执行相应的脚本
+if [[ $arch == "x86_64" ]]; then
+    echo "-----------  clickhouse is starting : [ $arch ]------------"
+    docker-compose -f ./docker-compose_amd64.yml down
+elif [[ $arch == "aarch64" ]]; then
+    echo "-----------  clickhouse is starting : [ $arch ]------------"
+    docker-compose -f ./docker-compose_arm64.yml down
+else
+    echo "Unsupported architecture: $arch"
+fi
